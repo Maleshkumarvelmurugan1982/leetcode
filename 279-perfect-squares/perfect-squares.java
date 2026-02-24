f649f5d91ec6a835c262fa527009d4c9bf2439ca
Maleshkumar.V <v.maleshkumar26032006@gmail.com>
2026-02-24 07:00:45 +0530

    Time: 35 ms (56.47%) | Memory: 44.6 MB (65.16%) - LeetSync

---
+++ 279-perfect-squares/perfect-squares.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int numSquares(int n) {
+        int[] dp = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            dp[i] = i;
+        }
+        dp[0] = 0;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j * j <= i; j++) {
+                dp[i] = Math.min(dp[i], dp[i - (j * j)] + 1);
+            }
+        }
+        return dp[n];
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int n=5; 
+        System.out.println(sol.numSquares(n));
+    }
+}
