87ccb476060fd8bc65bbca8147fab642d0d11837
Maleshkumar.V <v.maleshkumar26032006@gmail.com>
2026-04-04 10:48:13 +0530

    Time: 2 ms (30.70%) | Memory: 43.4 MB (20.75%) - LeetSync

---
+++ 520-detect-capital/detect-capital.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean detectCapitalUse(String word) {
+        if (word.length() < 2) return true;
+        
+        if (word.toUpperCase().equals(word)) return true;
+        if (word.toLowerCase().equals(word)) return true;
+        
+        if (Character.isUpperCase(word.charAt(0)) && 
+            word.substring(1).toLowerCase().equals(word.substring(1))) {
+            return true;
+        }
+        
+        return false;
+    }
+}
