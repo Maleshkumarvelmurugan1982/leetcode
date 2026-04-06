5e399c34de17b712ddc4d54facd77ef084556436
Maleshkumar.V <v.maleshkumar26032006@gmail.com>
2026-04-06 08:10:08 +0530

    Time: 3 ms (66.16%) | Memory: 47.3 MB (95.20%) - LeetSync

---
+++ 645-set-mismatch/set-mismatch.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] findErrorNums(int[] nums) {
+        int[] res = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            int val = Math.abs(nums[i]);
+            if (nums[val - 1] < 0) {
+                res[0] = val;
+            } else {
+                nums[val - 1] *= -1;
+            }
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) {
+                res[1] = i + 1;
+            }
+        }
+        return res;
+    }
+}
