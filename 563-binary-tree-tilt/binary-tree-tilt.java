461d1320864082138a585f8d5a6073b479118cbf
Maleshkumar.V <v.maleshkumar26032006@gmail.com>
2026-04-06 07:27:52 +0530

    Time: 0 ms (100.00%) | Memory: 46.1 MB (96.85%) - LeetSync

---
+++ 563-binary-tree-tilt/binary-tree-tilt.java
@@ -0,0 +1,19 @@
+class Solution {
+    int totalTilt = 0;
+
+    public int findTilt(TreeNode root) {
+        calculateSum(root);
+        return totalTilt;
+    }
+
+    private int calculateSum(TreeNode node) {
+        if (node == null) return 0;
+
+        int leftSum = calculateSum(node.left);
+        int rightSum = calculateSum(node.right);
+
+        totalTilt += Math.abs(leftSum - rightSum);
+
+        return node.val + leftSum + rightSum;
+    }
+}
