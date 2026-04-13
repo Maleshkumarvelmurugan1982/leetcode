6bdacdd768761fd1da2c85298529e352f193c93c
Maleshkumar.V <v.maleshkumar26032006@gmail.com>
2026-04-13 20:21:18 +0530

    Time: 2 ms (98.84%) | Memory: 43.4 MB (46.61%) - LeetSync

---
+++ 822-unique-morse-code-words/unique-morse-code-words.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int uniqueMorseRepresentations(String[] words) {
+        String[] morse = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+        
+        java.util.HashSet<String> seen = new java.util.HashSet<>();
+        for (String word : words) {
+            StringBuilder sb = new StringBuilder();
+            for (char c : word.toCharArray()) {
+                sb.append(morse[c - 'a']);
+            }
+            seen.add(sb.toString());
+        }
+        return seen.size();
+    }
+}
