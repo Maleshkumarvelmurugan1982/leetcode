35f231aa81430bd1aedd7925f2026a008f07974b
Maleshkumar.V <v.maleshkumar26032006@gmail.com>
2026-05-14 12:07:02 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (98.84%) - LeetSync

---
+++ 1514-minimum-value-to-get-positive-step-by-step-sum/minimum-value-to-get-positive-step-by-step-sum.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int minStartValue(int[] nums) {
+        int minPrefixSum = 0;
+        int currentSum = 0;
+        
+        for (int num : nums) {
+            currentSum += num;
+            if (currentSum < minPrefixSum) {
+                minPrefixSum = currentSum;
+            }
+        }
+        
+        return 1 - minPrefixSum;
+    }
+}
