683f1fa112b360bdaf79d049c7b557394896c9c9
Maleshkumar.V <v.maleshkumar26032006@gmail.com>
2026-05-14 11:52:50 +0530

    Time: 6 ms (99.62%) | Memory: 47 MB (8.75%) - LeetSync

---
+++ 1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] sortByBits(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
+            arr[i] += Integer.bitCount(arr[i]) * 100001;
+        }
+        
+        Arrays.sort(arr);
+        
+        for (int i = 0; i < n; i++) {
+            arr[i] %= 100001;
+        }
+        
+        return arr;
+    }
+}
