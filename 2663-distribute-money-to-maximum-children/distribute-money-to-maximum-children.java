e6966d0bf0fcbda2524e77ba18c7084d82e360f1
Maleshkumar.V <v.maleshkumar26032006@gmail.com>
2026-07-12 08:27:32 +0530

    Time: 2 ms (97.83%) | Memory: 43.7 MB (47.28%) - LeetSync

---
+++ 2663-distribute-money-to-maximum-children/distribute-money-to-maximum-children.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int distMoney(int money, int children) {
+        if(money<children){
+            return -1;
+        }
+        money-=children;
+        int count = Math.min(money/7, children);
+        money-=7*count;
+        children-=count;
+        if(children==1 && money==3){
+            count--;
+        }
+        if (children == 0 && money > 0) {
+            count--;
+
+        }
+        return count;
+    }
+}
